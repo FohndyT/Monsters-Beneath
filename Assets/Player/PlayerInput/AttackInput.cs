81d51148fdc77e9476fdd0e0039c871875b3a8da
Fohndy Tah <fohndytah@gmail.com>
2023-02-15 16:33:05 -0500

    Player added (2)

---
+++ Assets/Player/PlayerInput/AttackInput.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackInput : MonoBehaviour
+{
+    [SerializeField] private GameObject Sword;
+    [SerializeField] private Transform positionAttack;
+
+    private void OnAttack()
+    {
+        
+        Instantiate(Sword, positionAttack);
+    }
+}
