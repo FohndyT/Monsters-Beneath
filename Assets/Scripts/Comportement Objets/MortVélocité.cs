f4c1c71dedac6c8760117342bbd410e75822b486
Fohndy Tah <fohndytah@gmail.com>
2023-03-22 09:01:50 -0400

    Shaders et donjon

---
+++ Assets/Scripts/Comportement Objets/MortVélocité.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MortVélocité : MonoBehaviour
+{
+
+    [SerializeField] private float vitesseMort = 20f;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" && collision.gameObject.GetComponent<Rigidbody>().velocity.y > vitesseMort)
+        {
+            Debug.Log("Player is dead");
+        }
+    }
+}
