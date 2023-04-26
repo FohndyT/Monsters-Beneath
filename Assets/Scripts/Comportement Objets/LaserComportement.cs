e060e39f6e12e7669b2aef8b97ad47a53c97f21b
Fohndy Tah <fohndytah@gmail.com>
2023-04-26 10:42:42 -0400

    Pour pull

--- Assets/Scripts/Comportement Objets/LaserComportement.cs
+++ Assets/Scripts/Comportement Objets/LaserComportement.cs
@@ -6,6 +6,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+// https://www.youtube.com/watch?v=kuBIeQuqB20&t=151s&ab_channel=Loafwad
 // https://www.youtube.com/watch?v=kzHNUT9q4JE&ab_channel=BaDuy
 // https://www.google.com/search?q=how+to+refract+a+ray+unity&rlz=1C1ONGR_frCA1022CA1022&oq=how+to+refract+a+ray+unity&aqs=chrome..69i57j33i160l2.15732j0j7&sourceid=chrome&ie=UTF-8#fpstate=ive&vld=cid:0d0786ef,vid:r0Oz8FiiSUI
 
@@ -53,9 +54,6 @@ public class LaserComportement : MonoBehaviour
                 }
                 if (frappe.collider.CompareTag("Surface Réfractante"))
                 {
-                    float angle = CalculerAngleRefraction(Vector3.Angle(rayon.direction, frappe.normal) * Mathf.Deg2Rad);
-                    //Vector3 direction = new Vector3(Mathf.Cos(angle),0,Mathf.Sin(angle));
-
                     frappe.point = new Vector3(Mathf.Abs(CalculerVecteurRefraction().x) / (CalculerVecteurRefraction().x + 0.0001f) * 0.001f + frappe.point.x,
                                                Mathf.Abs(CalculerVecteurRefraction().y) / (CalculerVecteurRefraction().y + 0.0001f) * 0.001f + frappe.point.y,
                                                Mathf.Abs(CalculerVecteurRefraction().z) / (CalculerVecteurRefraction().z + 0.0001f) * 0.001f + frappe.point.z);
@@ -82,29 +80,5 @@ public class LaserComportement : MonoBehaviour
         {
             return (1/indiceDeRefraction * Vector3.Cross(frappe.normal, Vector3.Cross(-frappe.normal,rayon.direction)) - frappe.normal * Mathf.Sqrt(1-Vector3.Dot(Vector3.Cross(frappe.normal,rayon.direction) * (1/indiceDeRefraction * 1/indiceDeRefraction),Vector3.Cross(frappe.normal, rayon.direction)))).normalized;
         }
-        
-        /////////////////////////////////////////////////////////////////////////////////////////
-        
-        /* if (Physics.Raycast(transform.position,transform.forward, out frappe))
-        {
-            if (frappe.collider)
-            {
-                lr.SetPosition(1,frappe.point);
-            }
-        }
-        else
-        {
-            lr.SetPosition(1,transform.position + transform.forward * 500);
-        } 
-        
-        // Juste pour les tests, à enlever si implimenté dans le jeu
-        if (Input.GetKey("a"))
-        {
-            transform.Rotate(-Vector3.up / 3,Space.Self);
-        }
-        if (Input.GetKey("d"))
-        {
-            transform.Rotate(Vector3.up / 3,Space.Self);
-        } */
     }
 }
