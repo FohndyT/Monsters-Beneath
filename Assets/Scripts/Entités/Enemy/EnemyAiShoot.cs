88edec12d86a197f984a3439c963ea2fc307578f
Gabriel Chartier <gabrielchartier@outlook.com>
2023-05-01 10:13:34 -0400

    Enemy seek and shoot est implémenté et fonctionne, ne reste que de petit ajout. Le player ne devrait plus coller aux murs.

--- Assets/Scripts/Entités/Enemy/EnemyAiShoot.cs
+++ Assets/Scripts/Entités/Enemy/EnemyAiShoot.cs
@@ -2,13 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemyAiShoot : MonoBehaviour
 {
     [SerializeField] private Transform cible;
+    [SerializeField] private Transform shootT;
     public bool regardeJoueur = false;
     public bool peutRegarderJoueur = true;
+    private float shootCooldown = 0.75f;
     public RaycastHit rayHit;
+    [SerializeField] private GameObject bullet;
     public float rayHitMax { get; private set; }
 
     private void Awake()
@@ -18,15 +22,34 @@ public class EnemyAiShoot : MonoBehaviour
 
     void Update()
     {
-        if(regardeJoueur && peutRegarderJoueur)
+        if (regardeJoueur && peutRegarderJoueur)
+        {
             transform.LookAt(cible);
+            if (shootCooldown >= 0.75f)
+            {
+                BAM();
+                shootCooldown = 0f;
+            }
+        }
+
+        if (shootCooldown != 0.75f)
+            shootCooldown += Time.deltaTime;
+    }
+
+    private void BAM()
+    {
+        Vector3 shootPos = shootT.position;
+        shootT.position = new Vector3(shootPos.x * Random.Range(0.95f, 1.05f), shootPos.y * Random.Range(0.95f, 1.05f),
+            shootPos.z);
+        Instantiate(bullet, shootT);
+        shootT.DetachChildren();
     }
 
     private void OnTriggerStay(Collider other)
     {
         regardeJoueur = true;
         if(Physics.Raycast(transform.position, transform.forward, out rayHit, rayHitMax))
-            peutRegarderJoueur = rayHit.collider.tag != "Player";
+            peutRegarderJoueur = rayHit.collider.tag == "Player";
     }
     private void OnTriggerExit(Collider other)
     {
