4fcc302e179c17c0e1e448ef222a0473e8029543
Fohndy Tah <fohndytah@gmail.com>
2023-05-03 09:09:53 -0400

    Fix Cryptologie

--- Assets/Scripts/Cryptologie/Cryptologie.cs
+++ Assets/Scripts/Cryptologie/Cryptologie.cs
@@ -7,20 +7,20 @@ using UnityEngine.UI;
 public class Cryptologie : MonoBehaviour
 {
     [SerializeField]
-    public TMP_Text buttonTextNonCrypté1;
-    public TMP_Text buttonTextNonCrypté2;
-    public TMP_Text buttonTextNonCrypté3;
-    public TMP_Text buttonTextNonCrypté4;
-    public TMP_Text buttonTextNonCrypté5;
-    public TMP_Text buttonTextNonCrypté6;
-    public TMP_Text buttonTextNonCrypté7;
-    public TMP_Text buttonTextCrypté1;
-    public TMP_Text buttonTextCrypté2;
-    public TMP_Text buttonTextCrypté3;
-    public TMP_Text buttonTextCrypté4;
-    public TMP_Text buttonTextCrypté5;
-    public TMP_Text buttonTextCrypté6;
-    public TMP_Text buttonTextCrypté7;
+    public TMP_Text buttonTextNonCrypte1;
+    public TMP_Text buttonTextNonCrypte2;
+    public TMP_Text buttonTextNonCrypte3;
+    public TMP_Text buttonTextNonCrypte4;
+    public TMP_Text buttonTextNonCrypte5;
+    public TMP_Text buttonTextNonCrypte6;
+    public TMP_Text buttonTextNonCrypte7;
+    public TMP_Text buttonTextCrypte1;
+    public TMP_Text buttonTextCrypte2;
+    public TMP_Text buttonTextCrypte3;
+    public TMP_Text buttonTextCrypte4;
+    public TMP_Text buttonTextCrypte5;
+    public TMP_Text buttonTextCrypte6;
+    public TMP_Text buttonTextCrypte7;
 
     private string[] lettres = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
     public int[] IndiceDeLettreParBoutons = new int[14];
@@ -28,44 +28,44 @@ public class Cryptologie : MonoBehaviour
     public void AvancerDUneLettre (int indice)
     {
         IndiceDeLettreParBoutons[indice] = (IndiceDeLettreParBoutons[indice] + 1) % lettres.Length;
-        buttonTextNonCrypté1.text = lettres[IndiceDeLettreParBoutons[indice]];
+        buttonTextNonCrypte1.text = lettres[IndiceDeLettreParBoutons[indice]];
     }
     public void CrypterUneLettre(int indice)
     {
         IndiceDeLettreParBoutons[indice] = (IndiceDeLettreParBoutons[indice] + 3) % lettres.Length;
-        buttonTextCrypté1.text = lettres[IndiceDeLettreParBoutons[indice]];
+        buttonTextCrypte1.text = lettres[IndiceDeLettreParBoutons[indice]];
     }
-    public void OnClickBouttonNonCrypté1()
+    public void OnClickBouttonNonCrypte1()
     {
         AvancerDUneLettre(0);
         CrypterUneLettre(7);
     }
-    public void OnClickBouttonNonCrypté2()
+    public void OnClickBouttonNonCrypte2()
     {
         AvancerDUneLettre(1);
         CrypterUneLettre(8);
     }
-    public void OnClickBouttonNonCrypté3()
+    public void OnClickBouttonNonCrypte3()
     {
         AvancerDUneLettre(2);
         CrypterUneLettre(9);
     }
-    public void OnClickBouttonNonCrypté4()
+    public void OnClickBouttonNonCrypte4()
     {
         AvancerDUneLettre(3);
         CrypterUneLettre(10);
     }
-    public void OnClickBouttonNonCrypté5()
+    public void OnClickBouttonNonCrypte5()
     {
         AvancerDUneLettre(4);
         CrypterUneLettre(11);
     }
-    public void OnClickBouttonNonCrypté6()
+    public void OnClickBouttonNonCrypte6()
     {
         AvancerDUneLettre(5);
         CrypterUneLettre(12);
     }
-    public void OnClickBouttonNonCrypté7()
+    public void OnClickBouttonNonCrypte7()
     {
         AvancerDUneLettre(6);
         CrypterUneLettre(13);
