62a41eff1d01729345cc516c3afb105a926af00f
Imane El Hammar <202128397@edu.clg.qc.ca>
2023-05-04 14:31:18 -0400

    cryptologie completement corrige

--- Assets/Scripts/Cryptologie/Cryptologie.cs
+++ Assets/Scripts/Cryptologie/Cryptologie.cs
@@ -25,49 +25,59 @@ public class Cryptologie : MonoBehaviour
     private string[] lettres = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
     public int[] IndiceDeLettreParBoutons = new int[14];
 
-    public void AvancerDUneLettre(int indice, TMP_Text button)
+    public string AvancerDUneLettre(int indice, TMP_Text button)
     {
         IndiceDeLettreParBoutons[indice] = (IndiceDeLettreParBoutons[indice] + 1) % lettres.Length;
-        button.text = lettres[IndiceDeLettreParBoutons[indice]];
+        return lettres[IndiceDeLettreParBoutons[indice]];
+    }
+    public void AvancerDuneLettre2 ( TMP_Text button)
+    {
+        char buttonChar = (char)(((button.text.ToCharArray()[0] - 'a') + 1) % 26 + 'a');
+        button.text = buttonChar.ToString();
     }
     public void CrypterUneLettre(int indice, TMP_Text button)
     {
         IndiceDeLettreParBoutons[indice] = (IndiceDeLettreParBoutons[indice] + 3) % lettres.Length;
-        button.text = lettres[IndiceDeLettreParBoutons[indice]];
+        button.text= lettres[IndiceDeLettreParBoutons[indice]];
+    }
+    public void CrypterUneLettre2( TMP_Text button1, TMP_Text button2)
+    {
+       char buttonChar =(char) (((button1.text.ToCharArray()[0] -'a') + 3) % 26 + 'a') ;
+        button2.text = buttonChar.ToString();
     }
     public void OnClickBouttonNonCrypted1()
     {
-        AvancerDUneLettre(0, buttonTextNonCrypted1);
-        CrypterUneLettre(7, buttonTextCrypted1);
+        AvancerDuneLettre2(buttonTextNonCrypted1);
+        CrypterUneLettre2(buttonTextNonCrypted1, buttonTextCrypted1);  
     }
     public void OnClickBouttonNonCrypted2()
     {
-        AvancerDUneLettre(1, buttonTextNonCrypted2);
-        CrypterUneLettre(8, buttonTextCrypted2);
+        AvancerDuneLettre2(buttonTextNonCrypted2);
+        CrypterUneLettre2(buttonTextNonCrypted2 , buttonTextCrypted2);
     }
     public void OnClickBouttonNonCrypted3()
     {
-        AvancerDUneLettre(2, buttonTextNonCrypted3);
-        CrypterUneLettre(9, buttonTextCrypted3);
+        AvancerDuneLettre2(buttonTextNonCrypted3);
+        CrypterUneLettre2(buttonTextNonCrypted3, buttonTextCrypted3);
     }
     public void OnClickBouttonNonCrypted4()
     {
-        AvancerDUneLettre(3, buttonTextNonCrypted4);
-        CrypterUneLettre(10, buttonTextCrypted4);
+        AvancerDuneLettre2( buttonTextNonCrypted4);
+        CrypterUneLettre2(buttonTextNonCrypted4, buttonTextCrypted4);
     }
     public void OnClickBouttonNonCrypted5()
     {
-        AvancerDUneLettre(4, buttonTextNonCrypted5);
-        CrypterUneLettre(11, buttonTextCrypted5);
+        AvancerDuneLettre2(buttonTextNonCrypted5);
+        CrypterUneLettre2(buttonTextNonCrypted5, buttonTextCrypted5);
     }
     public void OnClickBouttonNonCrypted6()
     {
-        AvancerDUneLettre(5, buttonTextNonCrypted6);
-        CrypterUneLettre(12, buttonTextCrypted6);
+        AvancerDuneLettre2(buttonTextNonCrypted6);
+        CrypterUneLettre2(buttonTextCrypted6, buttonTextCrypted6);
     }
     public void OnClickBouttonNonCrypted7()
     {
-        AvancerDUneLettre(6, buttonTextNonCrypted7);
-        CrypterUneLettre(13, buttonTextCrypted7);
+        AvancerDuneLettre2(buttonTextNonCrypted7);
+        CrypterUneLettre2(buttonTextNonCrypted7, buttonTextCrypted7);
     }
 }
