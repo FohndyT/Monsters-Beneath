37e468799e270f82464dbea85f8b14cab50f1c91
Imane El Hammar <202128397@edu.clg.qc.ca>
2023-05-04 13:38:47 -0400

    Cryptage semi corrigé

--- Assets/Scripts/Cryptologie/Cryptologie.cs
+++ Assets/Scripts/Cryptologie/Cryptologie.cs
@@ -7,67 +7,67 @@ using UnityEngine.UI;
 public class Cryptologie : MonoBehaviour
 {
     [SerializeField]
-    public TMP_Text buttonTextNonCrypte1;
-    public TMP_Text buttonTextNonCrypte2;
-    public TMP_Text buttonTextNonCrypte3;
-    public TMP_Text buttonTextNonCrypte4;
-    public TMP_Text buttonTextNonCrypte5;
-    public TMP_Text buttonTextNonCrypte6;
-    public TMP_Text buttonTextNonCrypte7;
-    public TMP_Text buttonTextCrypte1;
-    public TMP_Text buttonTextCrypte2;
-    public TMP_Text buttonTextCrypte3;
-    public TMP_Text buttonTextCrypte4;
-    public TMP_Text buttonTextCrypte5;
-    public TMP_Text buttonTextCrypte6;
-    public TMP_Text buttonTextCrypte7;
+    public TMP_Text buttonTextNonCrypted1;
+    public TMP_Text buttonTextNonCrypted2;
+    public TMP_Text buttonTextNonCrypted3;
+    public TMP_Text buttonTextNonCrypted4;
+    public TMP_Text buttonTextNonCrypted5;
+    public TMP_Text buttonTextNonCrypted6;
+    public TMP_Text buttonTextNonCrypted7;
+    public TMP_Text buttonTextCrypted1;
+    public TMP_Text buttonTextCrypted2;
+    public TMP_Text buttonTextCrypted3;
+    public TMP_Text buttonTextCrypted4;
+    public TMP_Text buttonTextCrypted5;
+    public TMP_Text buttonTextCrypted6;
+    public TMP_Text buttonTextCrypted7;
 
     private string[] lettres = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
     public int[] IndiceDeLettreParBoutons = new int[14];
 
-    public void AvancerDUneLettre (int indice)
+    public void AvancerDUneLettre(int indice, TMP_Text button)
     {
         IndiceDeLettreParBoutons[indice] = (IndiceDeLettreParBoutons[indice] + 1) % lettres.Length;
-        buttonTextNonCrypte1.text = lettres[IndiceDeLettreParBoutons[indice]];
+        button.text = lettres[IndiceDeLettreParBoutons[indice]];
     }
-    public void CrypterUneLettre(int indice)
+    public void CrypterUneLettre(int indice, TMP_Text button)
     {
         IndiceDeLettreParBoutons[indice] = (IndiceDeLettreParBoutons[indice] + 3) % lettres.Length;
-        buttonTextCrypte1.text = lettres[IndiceDeLettreParBoutons[indice]];
+        button.text = lettres[IndiceDeLettreParBoutons[indice]];
     }
-    public void OnClickBouttonNonCrypte1()
+    public void OnClickBouttonNonCrypted1()
     {
-        AvancerDUneLettre(0);
-        CrypterUneLettre(7);
+        AvancerDUneLettre(0, buttonTextNonCrypted1);
+        CrypterUneLettre(7, buttonTextCrypted1);
     }
-    public void OnClickBouttonNonCrypte2()
+    public void OnClickBouttonNonCrypted2()
     {
-        AvancerDUneLettre(1);
-        CrypterUneLettre(8);
+        AvancerDUneLettre(1, buttonTextNonCrypted2);
+        CrypterUneLettre(8, buttonTextCrypted2);
     }
-    public void OnClickBouttonNonCrypte3()
+    public void OnClickBouttonNonCrypted3()
     {
-        AvancerDUneLettre(2);
-        CrypterUneLettre(9);
+        AvancerDUneLettre(2, buttonTextNonCrypted3);
+        CrypterUneLettre(9, buttonTextCrypted3);
     }
-    public void OnClickBouttonNonCrypte4()
+    public void OnClickBouttonNonCrypted4()
     {
-        AvancerDUneLettre(3);
-        CrypterUneLettre(10);
+        AvancerDUneLettre(3, buttonTextNonCrypted4);
+        CrypterUneLettre(10, buttonTextCrypted4);
     }
-    public void OnClickBouttonNonCrypte5()
+    public void OnClickBouttonNonCrypted5()
     {
-        AvancerDUneLettre(4);
-        CrypterUneLettre(11);
+        AvancerDUneLettre(4, buttonTextNonCrypted5);
+        CrypterUneLettre(11, buttonTextCrypted5);
     }
-    public void OnClickBouttonNonCrypte6()
+    public void OnClickBouttonNonCrypted6()
     {
-        AvancerDUneLettre(5);
-        CrypterUneLettre(12);
+        AvancerDUneLettre(5, buttonTextNonCrypted6);
+        CrypterUneLettre(12, buttonTextCrypted6);
     }
-    public void OnClickBouttonNonCrypte7()
+    public void OnClickBouttonNonCrypted7()
     {
-        AvancerDUneLettre(6);
-        CrypterUneLettre(13);
+        AvancerDUneLettre(6, buttonTextNonCrypted7);
+        CrypterUneLettre(13, buttonTextCrypted7);
     }
 }
