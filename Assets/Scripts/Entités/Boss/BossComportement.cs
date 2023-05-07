8ca3dbf20601960eb3c2ac29eafbb771aa960956
Fohndy Tah <fohndytah@gmail.com>
2023-05-07 00:23:08 -0400

    Onde de choc boss

--- Assets/Scripts/Entités/Boss/BossComportement.cs
+++ Assets/Scripts/Entités/Boss/BossComportement.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using Unity.VisualScripting;
 using UnityEditor.Animations;
 using UnityEngine;
@@ -38,16 +39,13 @@ public class BossComportement : MonoBehaviour
     private int vieRestante;
     [SerializeField] private float vitesseMouvement = 5f;
 
-    private GameObject joueur;
-    private Animator animation;
-
-    private bool estProchePourSaut;
-    private bool marche = true;
-    private bool sautEnMarche;
-    private bool PeutSauter = true;
-    private bool RochesMontantesEnMarche;
-
     private Rigidbody rb;
+    private Animator animation;
+    private GameObject joueur;
+    private Rigidbody rbJoueur;
+
+    private bool EstEnTrainDeMarcher = true;
+    private bool peutGenererOnde = true;
     
     private float temps;
     private Vector3 positionJoueurDash;
@@ -60,6 +58,7 @@ public class BossComportement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         joueur = GameObject.Find("Player");
+        rbJoueur = joueur.GetComponent<Rigidbody>();
         animation = GetComponent<Animator>();
 
         vieRestante = vieMax;
@@ -71,21 +70,27 @@ public class BossComportement : MonoBehaviour
         attaquePiedsGauche = GameObject.Find("B-toe.L").GetComponent<DommageBoss>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         RegarderJoueur();
         
         if (phase == 1)
         {
             PoursuitePhaseUn();
-            Sauter();
+            OndeDeChoc();
         }
 
         if (phase == 2)
         {
             PoursuitePhaseDeux();
+            OndeDeChoc();
         }
 
+        if (vieRestante <= 400f)
+        {
+            phase = 2;
+        }
+        
         if (vieRestante == 0)
         {
             
@@ -97,7 +102,7 @@ public class BossComportement : MonoBehaviour
             PrendreDegats(20);
         }
     }
-    void PrendreDegats(int degats)
+    public void PrendreDegats(int degats)
     {
         vieRestante -= degats;
         barDeVie.MettreVie(vieRestante);
@@ -106,7 +111,7 @@ public class BossComportement : MonoBehaviour
     #region Mouvements
     void RegarderJoueur()
     {
-        if (marche)
+        if (EstEnTrainDeMarcher)
         {
             transform.LookAt(new Vector3(joueur.transform.position.x,transform.position.y,joueur.transform.position.z));
         }
@@ -117,7 +122,7 @@ public class BossComportement : MonoBehaviour
     }
     void PoursuitePhaseUn()
     {
-        if (marche)
+        if (EstEnTrainDeMarcher)
         {
             transform.position = Vector3.MoveTowards(transform.position, new Vector3(joueur.transform.position.x, transform.position.y, joueur.transform.position.z), vitesseMouvement * Time.deltaTime);
             
@@ -126,7 +131,7 @@ public class BossComportement : MonoBehaviour
     }
     void PoursuitePhaseDeux()
     {
-        if (marche)
+        if (EstEnTrainDeMarcher)
         {
             transform.position = Vector3.MoveTowards(transform.position,
             new Vector3(joueur.transform.position.x, transform.position.y, joueur.transform.position.z), vitesseMouvement * 1.5f * Time.deltaTime);
@@ -139,11 +144,9 @@ public class BossComportement : MonoBehaviour
     #region AttaqueCorpsACorps
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.name == "Player" && sautEnMarche == false)
+        if (other.gameObject.name == "Player")
         {
-            Debug.Log("Trigger Activated");
-            marche = false;
-            estProchePourSaut = true;
+            EstEnTrainDeMarcher = false;
             
             attaqueMainGauche.estActive = true;
             attaqueMainDroite.estActive = true;
@@ -156,57 +159,41 @@ public class BossComportement : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            this.Attendre(2f, () => { marche = true;});
+            this.Attendre(2f, ()=> {EstEnTrainDeMarcher = true;});
             
             attaqueMainGauche.estActive = false;
             attaqueMainDroite.estActive = false;
             attaquePiedsGauche.estActive = false;
-
-            estProchePourSaut = false;
         }
     }
     #endregion
 
     #region Attaques
-    void Sauter()
+    void OndeDeChoc()
     {
-        if (estProchePourSaut && PeutSauter)
+        if (DistanceEntreBossJoueur() <= 7f && peutGenererOnde)
         {
-            this.Attendre(7f, () =>
+            EstEnTrainDeMarcher = false;
+
+            animation.runtimeAnimatorController = animationCreationSlime;
+            this.Attendre(2f, () =>
             {
-                while (estProchePourSaut)
-                {
-                    sautEnMarche = true;
-                    marche = false;
-                    
-                    animation.runtimeAnimatorController = animationSauter;
-                    rb.AddForce(new Vector3(0,15000f,0),ForceMode.Impulse);
-                    PeutSauter = false;
-                    
-                    
-                    this.Attendre(3f, () => { GameObject cloneOnde = Instantiate(ondeDeShoc,transform.position,transform.rotation); this.Attendre(1f, ()=> Destroy(cloneOnde));});
-                    this.Attendre(4f, () => { sautEnMarche = false;});
-                    this.Attendre(4f, () => { marche = true;});
-                    this.Attendre(10f, () => { PeutSauter = true;});
-                    PeutSauter = false;
-                    estProchePourSaut = false;
-                }
+                GameObject cloneOndeDeChoc = Instantiate(ondeDeShoc,transform.position,transform.rotation);
+                Vector3 directionForce = (transform.position - joueur.transform.position).normalized;
+                directionForce.y = 0;
+                rbJoueur.AddForce(10000f * directionForce, ForceMode.Impulse);
+                this.Attendre(1.7f, () => { Destroy(cloneOndeDeChoc);});
             });
+
+            peutGenererOnde = false;
+            this.Attendre(4f, () => { EstEnTrainDeMarcher = true;});
+            this.Attendre(15f, () => { peutGenererOnde = true;});
         }
     }
     #endregion
-    
-    void MettreRochesMontantesEnFalse()
+
+    private float DistanceEntreBossJoueur()
     {
-        RochesMontantesEnMarche = false;
-    }
-    void MettreSautEnFalse()
-    {
-        sautEnMarche = false;
-        attaqueCorps.estActive = false;
-    }
-    private void MarcheEnTrue()
-    {
-        marche = true;
+        return Vector3.Distance(transform.position, joueur.transform.position);
     }
 }
